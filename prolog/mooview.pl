f6b0c769c0e93cac530ba5d0c4e166b9ad3a6493
Gabriel Costa <gabriel.costa@ccc.ufcg.edu.br>
2021-05-15 00:15:01 -0300

    Init prolog project

--- prolog/mooview.pl
+++ prolog/mooview.pl
@@ -1,19 +1,18 @@
 /* 	
 	MOOVIEW - UMA PLATAFORMA PARA RECOMENDAÇÃO E AVALIAÇÃO DE FILMES
 	PARADIGMAS DE LINGUAGEM DE PROGRAMACAO - 2020.1e - UFCG
-    
+
 	INTEGRANTES:
                 GABRIEL FERNANDES DA COSTA
                 ERICK MORAIS DE SENA
                 DANIEL DE MATOS FIGUEREDO
                 NATAN VINICIUS DA SILVA LUCENA
 */
-
-:- [view].
-
+:- include('view.pl').
 
 :- initialization(main).
 
 main :-
     slogan(),nl,
-    halt(o).
+    loading(),
+    logOpt(),nl.
